7bda138c74bc0e7f0689f6e4f920f4da052cd58a
Loren Niu <lorentiffanyniu@cen-0033942.local>
2018-05-29 16:58:35 -0400

    Initial Commit

---
+++ Loren App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loren App
+//
+//  Created by Loren Niu on 5/29/18.
+//  Copyright © 2018 Loren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
